612d159f05e8b598aca9a5eb69cc0f8172004a22
Wikilipo <alexmolinaperez1@gmail.com>
2025-03-12 11:52:46 -0400

    Prueba de superficie de pantalla

--- prueba.py
+++ prueba.py
@@ -4,11 +4,21 @@ from sys import exit #usando funcion specifica (from en vez de import)
 pygame.init()
 screen = pygame.display.set_mode((400,400))
 pygame.display.set_caption('Snake') #nombre d eel window
+clock = pygame.time.Clock()
+
+test_surface = pygame.Surface((400,400)) #origin is alawys top left (0,0)
+test_surface.fill('blue')
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit() #opposite to pygame.init()
             exit()
+
+    screen.blit(test_surface,(0,0))
     #draw and update every element
     pygame.display.update()
+    clock.tick(60) #60 times per second (max framerate)
+
+
+
