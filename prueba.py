f2eee34b43f0db36aef94674c2859cf62fc06e28
Wikilipo <alexmolinaperez1@gmail.com>
2025-03-12 11:36:42 -0400

    implementando window, exit y initialize

--- prueba.py
+++ prueba.py
@@ -1,1 +1,14 @@
-print('hello world')
+import pygame
+from sys import exit #usando funcion specifica (from en vez de import)
+
+pygame.init()
+screen = pygame.display.set_mode((400,400))
+pygame.display.set_caption('Snake') #nombre d eel window
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit() #opposite to pygame.init()
+            exit()
+    #draw and update every element
+    pygame.display.update()
