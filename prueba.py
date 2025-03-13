35376640058f843ad674451e60727a59ed72b02e
Wikilipo <alexmolinaperez1@gmail.com>
2025-03-13 13:52:22 -0400

    Player Surface

--- prueba.py
+++ prueba.py
@@ -5,8 +5,13 @@ pygame.init()
 screen = pygame.display.set_mode((700,600))
 pygame.display.set_caption('Snake') #nombre d eel window
 clock = pygame.time.Clock()
+test_score_font= pygame.font.Font(None, 50)
 
-background = pygame.image.load('img/SnakeBack.jpg') #origin is alawys top left (0,0)
+background = pygame.image.load('img/SnakeBack.jpg').convert() #origin is alawys top left (0,0)
+text_surface = test_score_font.render('Score',False,'Red')
+
+player_surf = pygame.image.load('img/Player_Square.webp')
+player_rect= player_surf.get_rect(midleft=(100,300)) #rect allows for specific point of movement
 
 while True:
     for event in pygame.event.get():
@@ -15,6 +20,8 @@ while True:
             exit()
 
     screen.blit(background,(0,0)) #blit = draw me to that location
+    screen.blit(text_surface,(300,15))
+    screen.blit(player_surf,player_rect) #The axis is now the previosly defined rect
     #draw and update every element
     pygame.display.update()
     clock.tick(60) #60 times per second (max framerate)
