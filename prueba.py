af4b6b0936710bed4fc60efc512e68d044fb5bbc
Wikilipo <alexmolinaperez1@gmail.com>
2025-03-12 21:37:29 -0400

    Background

--- prueba.py
+++ prueba.py
@@ -2,12 +2,11 @@ import pygame
 from sys import exit #usando funcion specifica (from en vez de import)
 
 pygame.init()
-screen = pygame.display.set_mode((400,400))
+screen = pygame.display.set_mode((700,600))
 pygame.display.set_caption('Snake') #nombre d eel window
 clock = pygame.time.Clock()
 
-test_surface = pygame.Surface((400,400)) #origin is alawys top left (0,0)
-test_surface.fill('blue')
+background = pygame.image.load('SnakeBack.jpg') #origin is alawys top left (0,0)
 
 while True:
     for event in pygame.event.get():
@@ -15,7 +14,7 @@ while True:
             pygame.quit() #opposite to pygame.init()
             exit()
 
-    screen.blit(test_surface,(0,0))
+    screen.blit(background,(0,0)) #blit = draw me to that location
     #draw and update every element
     pygame.display.update()
     clock.tick(60) #60 times per second (max framerate)
