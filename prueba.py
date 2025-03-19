0368ffb815b39ad00586b990bc0eccc777b9813c
Wikilipo <alexmolinaperez1@gmail.com>
2025-03-19 16:09:35 -0400

    Screen print (movement not working)

--- prueba.py
+++ prueba.py
@@ -8,7 +8,7 @@ pygame.display.set_caption('Snake') #nombre d eel window
 clock = pygame.time.Clock()
 test_score_font= pygame.font.Font(None, 50)
 
-background = pygame.image.load('img/SnakeBack.jpg').convert() #origin is alawys top left (0,0)
+#background = pygame.image.load('img/SnakeBack.jpg').convert() #origin is alawys top left (0,0)
 text_surface = test_score_font.render('Score',False,'black')
 
 # player_surf = pygame.image.load('img/player.png')
@@ -18,17 +18,20 @@ text_surface = test_score_font.render('Score',False,'black')
 # fruit_surf = pygame.transform.scale(fruit_surf,(50,50))
 
 #Define player and fruit
-player = pygame.Rect(0,0,100,100)
-fruit = pygame.Rect(0,0,300,300)
+player = pygame.Rect(100,300,50,50)
+fruit = pygame.Rect(random.randint(50, 650),random.randint(50,550),30,30) #random sapwn
 pygame.draw.rect(screen,(255,0,0),player)
 pygame.draw.rect(screen,(0,255,0),fruit)
 
 
 #fruit spawning 
+#def spawn_fruit():
+#    x = random.randint(100,700) #within playable width
+#    y = random.randint(100,600) # within playable height
+#    return pygame.Rect(x,y,85, 85) #fruit rect #25 so it doesnt instantly collide
+
 def spawn_fruit():
-    x = random.randint(100,700) #within playable width
-    y = random.randint(100,600) # within playable height
-    return pygame.Rect(x,y,85, 85) #fruit rect #25 so it doesnt instantly collide
+    return pygame.Rect(random.randint(50,650),random.randint(50,550),30,30)
 
 
 
@@ -46,7 +49,7 @@ while True:
         
         #key press check
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_UP and direction != "DOWN:":
+            if event.key == pygame.K_UP and direction != "DOWN":
                 direction = "UP"
             elif event.key == pygame.K_DOWN and direction != "UP":
                 direction = "DOWN"
@@ -68,7 +71,7 @@ while True:
 
 #New fruit for score
     if player.colliderect(fruit):
-        spawn_fruit()
+        fruit = spawn_fruit()
     #screen.blit(background,(0,0)) #blit = draw me to that location
     #screen.blit(text_surface,(300,15))
 
@@ -76,8 +79,10 @@ while True:
     #player_rect.x += 1
     if player.right >= 865: 
         player.left = 100
+        screen.fill((200,200,200)) #background gris
         pygame.draw.rect(screen, (0, 255, 0), player)
         pygame.draw.rect(screen, (255, 0, 0), fruit)
+        screen.blit(text_surface,(300,15))
     #screen.blit(player) #The axis is now the previosly defined rect
     #screen.blit(fruit_score_surf,fruit_rect)
 
