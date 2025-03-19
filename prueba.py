299d3925f990e65cc6464e47b8e936a0dfde2433
Wikilipo <alexmolinaperez1@gmail.com>
2025-03-19 08:28:59 -0400

    Player Movement (complete)

--- prueba.py
+++ prueba.py
@@ -13,6 +13,10 @@ text_surface = test_score_font.render('Score',False,'Red')
 player_surf = pygame.image.load('img/Player_Square.webp')
 player_rect= player_surf.get_rect(midleft=(100,300)) #rect allows for specific point of movement
 
+#Movement variables 
+
+direction = "RIGHT"
+speed = 4 
 #fruit_score_surf = pygame.image.load('img/3_hearts.png')
 #fruit_rect = fruit_score_surf.get_rect(midleft = (600,300))
 while True:
@@ -20,11 +24,34 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit() #opposite to pygame.init()
             exit()
+        
+        #key press check
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP and direction != "DOWN:":
+                direction = "UP"
+            elif event.key == pygame.K_DOWN and direction != "UP":
+                direction = "DOWN"
+            elif event.key == pygame.K_LEFT and direction != "RIGHT":
+                direction = "LEFT"
+            elif event.key == pygame.K_RIGHT and direction != "LEFT":
+                direction = "RIGHT"
+
+    #update movement
+    if direction == "UP":
+        player_rect.y -= speed
+    elif direction == "DOWN":
+        player_rect.y += speed
+    elif direction == "LEFT":
+        player_rect.x -= speed
+    elif direction == "RIGHT":
+        player_rect.x += speed
+    
 
     screen.blit(background,(0,0)) #blit = draw me to that location
     screen.blit(text_surface,(300,15))
 
-    player_rect.x += 1
+#default movement left 
+    #player_rect.x += 1
     if player_rect.right >= 865: player_rect.left = 100
     
     screen.blit(player_surf,player_rect) #The axis is now the previosly defined rect
@@ -34,6 +61,7 @@ while True:
     
     pygame.display.update()
     clock.tick(60) #60 times per second (max framerate)
+    #for smooth movement 
 
 
 
